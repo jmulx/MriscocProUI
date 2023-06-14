70100b2df24901126361957136d94d1e85a098ea
James Mullineux <j@withlight.co.uk>
2023-06-14 20:14:35 +0100

    tweak to probe offset and input shaping things

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1488,7 +1488,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -57.4, -11.4, -3.43 }
+#define NOZZLE_TO_PROBE_OFFSET { -57.4, -11.4, -3.84 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
